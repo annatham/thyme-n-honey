bed5574ca0ff7417b0280f2e2e581a8f9c7a961b
Anna Thammakane <anna.thammakane@gmail.com>
2019-11-09 13:07:41 -0600

    updating

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,36 @@
-var db = require("../models");
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
 
-module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
+// Dependencies
+// =============================================================
+var path = require("path");
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+// Routes
+// =============================================================
+module.exports = function(app) {
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  });
+
+  
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/about.html"));
+  });
+
+  // blog route loads blog.html
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
 };
+  // blog route loads blog.html
+  app.get("/recipe", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/recipe.html"));
+  });
+
+
